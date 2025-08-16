ae853f73806e8a7bc6780c1e7df1f7624bad0bff
Mohit Karn <mohitkarn123@gmail.com>
2025-08-16 18:44:43 +0530

    structure the program

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
+#imports
 from flask import Flask, render_template
+
+#create Flask app
 app = Flask(__name__) # serve homepage
+
+# routes
 @app.route('/')
 def index():
     return render_template('index.html')   
+
+
+#run the app
 if __name__ == '__main__':
     app.run(debug=True)
